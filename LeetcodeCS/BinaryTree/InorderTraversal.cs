b3ca4c75fe7a2c0a5a0b39d7ec4e4abcbd5e0c2a
Sergey Koptev <s.koptev@corp.nekki.ru>
2019-08-01 13:35:03 +0300

    Added solution from InorderTraversal

---
+++ LeetcodeCS/BinaryTree/InorderTraversal.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+public class SolutionInorderTraversal
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public static IList<int> InorderTraversal(TreeNode root) 
+    {
+        var res = new List<int>();
+        
+        if (root == null)
+            return res;        
+        
+        var curr = root;
+        var stack = new Stack<TreeNode>();
+        
+        while (true)
+        {
+            if (curr != null)
+            {
+                stack.Push(curr);
+                curr = curr.left;
+            }
+            else
+            {
+                if (stack.Count > 0)
+                {                
+                    var top = stack.Pop();
+                    res.Add(top.val);
+
+                    curr = top.right;
+                }
+                else
+                    break;
+            }
+        }
+        
+        return res;        
+    }
+
+    public static void Run()
+    {
+        var input = new TreeNode(1){right = new TreeNode(2){left=new TreeNode(3)}};
+
+        var output = InorderTraversal(input);
+
+        int f = 2;
+    }
+}
