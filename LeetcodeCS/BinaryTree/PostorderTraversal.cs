d8b1c8836590996e87428800d702d0e83a0e82fb
Sergey Koptev <s.koptev@corp.nekki.ru>
2019-08-15 13:32:51 +0300

    Solution for post-order traversal

--- LeetcodeCS/BinaryTree/PostorderTraversal.cs
+++ LeetcodeCS/BinaryTree/PostorderTraversal.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Xml;
+using System.Xml.Linq;
 
 public class SolutionPostorderTraversal
 {
@@ -17,13 +21,71 @@ public class SolutionPostorderTraversal
         if (root == null)
             return result;
 
+        var stack = new Stack<TreeNode>();
+        var curr = root;
+
+        while (true)
+        {
+            if (curr != null)
+            {
+                if (curr.left != null)
+                {
+                    if (curr.right != null)
+                        stack.Push(curr.right);
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                else
+                {
+                    if (curr.right != null)
+                    {
+                        stack.Push(curr);
+                        curr = curr.right;
+                    }
+                    else
+                    {
+                        result.Add(curr.val);
+                        curr = null;
+                    }
+                }
+            }
+            else
+            {
+                if (stack.Count == 0)
+                    break;
+
+                curr = stack.Pop();
+
+                if (curr.right != null && stack.Count > 0 && curr.right == stack.Peek())
+                {
+                    var r = stack.Pop();
+                    stack.Push(curr);
+                    curr = curr.right;
+                }
+                else
+                {
+                    result.Add(curr.val);
+                    curr = null;
+                }
+            }
+        }
+
 
         return result;
     }
 
     public static void Run()
     {
-        var input = new TreeNode(1){right = new TreeNode(2){left=new TreeNode(3)}};
+        //var input = new TreeNode(1)
+        //{
+        //    right = new TreeNode(3),
+        //    left = new TreeNode(2) { left = new TreeNode(4), right = new TreeNode(5)}
+        //};
+
+        var input = new TreeNode(1)
+        {
+            right = new TreeNode(2) {left = new TreeNode(3)}
+        };
 
         var output = PostorderTraversal(input);
     }
